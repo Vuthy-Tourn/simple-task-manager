c8ca246c86533e50cf379adfc3da0424b614ba5e
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-03-05 00:49:24 +0700

    Updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'standalone' }
+const nextConfig = {
+  output: 'standalone',
+  serverRuntimeConfig: {
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  },
+}
+
 module.exports = nextConfig
